bc3e3a28522e6fa6f0626710a67f7f56ad1b665b
Guido Schmitz <guido.schmitz@fedaix.de>
2020-11-05 16:29:38 +0000

    Add system_health platform

---
+++ homeassistant/components/devolo_home_control/system_health.py
@@ -0,0 +1,26 @@
+"""Provide info to system health."""
+from homeassistant.components import system_health
+from homeassistant.core import HomeAssistant, callback
+
+from .const import DOMAIN
+
+
+@callback
+def async_register(
+    hass: HomeAssistant, register: system_health.RegisterSystemHealth
+) -> None:
+    """Register system health callbacks."""
+    register.async_register_info(system_health_info)
+
+
+async def system_health_info(hass: HomeAssistant):
+    """Get info for the info page."""
+
+    data = {}
+    for entry in hass.data[DOMAIN]:
+        for gateway in hass.data[DOMAIN][entry]["gateways"]:
+            data[
+                f"{gateway.gateway.id}_local_connection"
+            ] = gateway.gateway.local_connection
+
+    return data
